67643a64af5662c2c1174471b03cdfc31eed05a0
iadokonat <iadokonat0106@gmail.com>
2023-07-16 03:12:38 +0900

    settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // basePath: '/vanilla-extract-sample',
+  // distDir: 'out/vanilla-extract-sample',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+}
 
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
-const withVanillaExtract = createVanillaExtractPlugin();
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
+const withVanillaExtract = createVanillaExtractPlugin()
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = withVanillaExtract(nextConfig)
