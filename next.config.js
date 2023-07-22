56a99b58be2691c268ce712b3f8be6819f15de6a
iadokonat <iadokonat0106@gmail.com>
2023-07-22 10:38:27 +0900

    fix: next.config.jsを修正

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   // basePath: '/vanilla-extract-sample',
   // distDir: 'out/vanilla-extract-sample',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
 }
 
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
