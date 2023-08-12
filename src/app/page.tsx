d9abde812c35f2873b0132f76691369319a0896a
iadokonat <iadokonat0106@gmail.com>
2023-08-12 20:10:56 +0900

    fix: バグを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,55 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Button } from './_components/Button'
 import { Table } from './_components/Table'
 import { useBodyTemp } from '@/hooks/useBodyTemp'
 
-import logoImage from '@/assets/images/logo.png'
-
 export default function Home() {
   const { isLoading } = useBodyTemp()
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null)
+  const [notInstalled, setNotInstalled] = useState<boolean>(false)
+
+  const handleClickInstallButton = async () => {
+    deferredPrompt.prompt()
+
+    const { outcome } = await deferredPrompt.userChoice
+
+    setDeferredPrompt(null)
+
+    if (outcome === 'accepted') {
+      console.log('User accepted the install prompt.')
+    } else if (outcome === 'dismissed') {
+      console.log('User dismissed the install prompt')
+    }
+  }
 
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/serviceworker.js')
+    }
 
+    if ('setAppBadge' in navigator) {
       let unreadCount = 125
       navigator.setAppBadge(unreadCount)
     }
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault()
+      setDeferredPrompt(e)
+      console.log(e)
+      setNotInstalled(true)
+    })
+
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches
+    if (isStandalone) {
+      console.log('standalone')
+    } else {
+      console.log('not standalone')
+    }
+
+    console.log(navigator)
   }, [])
 
   return (
@@ -29,6 +61,12 @@ export default function Home() {
           体温を記録する
         </Button>
       </div>
+
+      {notInstalled && (
+        <div className="my-8">
+          <Button onClick={handleClickInstallButton}>インストール</Button>
+        </div>
+      )}
     </>
   )
 }
