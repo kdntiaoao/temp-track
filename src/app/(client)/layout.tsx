e92e698d3eaec0bc2574262d06ffaac301d5c03a
iadokonat <iadokonat0106@gmail.com>
2023-08-27 15:40:10 +0900

    fix

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js')
     }
+    console.log(navigator)
 
     const isStandalone = window.matchMedia('(display-mode: standalone)').matches
     if (isStandalone) {
@@ -16,27 +17,34 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       console.log('not standalone')
     }
 
-    console.log(navigator)
+    // Initializing the OneSignal SDK
+    // https://onesignal.com/blog/how-to-integrate-onesignal-into-a-next-app/
+    window.OneSignal = window.OneSignal || []
+    const appId = 'b40b7cc7-13dc-4662-8b48-efa668f9b72a' // production
+    // const appId = 'edc80000-6aca-4a61-a7eb-227d8026f625' // dev
+    const safari_web_id = 'web.onesignal.auto.028d9952-ba2c-477b-babc-6aee5c5ba0de' // production
+    // const safari_web_id = 'web.onesignal.auto.44fa898b-ecac-43e4-a4f9-b7817452e5d5' // dev
+    window.OneSignal.push(function (OneSignal: any) {
+      OneSignal.init({
+        appId,
+        safari_web_id,
+        notifyButton: {
+          enable: true,
+        },
+
+        allowLocalhostAsSecureOrigin: true,
+      })
+    })
+    console.log('OneSignal', window.OneSignal)
+
+    return () => {
+      window.OneSignal = undefined
+    }
   }, [])
 
   return (
     <>
       <Script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer strategy="beforeInteractive" />
-      <Script id="one-signal" strategy="beforeInteractive">
-        {`
-          window.OneSignalDeferred = window.OneSignalDeferred || [];
-          OneSignalDeferred.push(function(OneSignal) {
-            OneSignal.init({
-              appId: "00e9639b-fa8a-4e79-8087-e071b16f9183",
-              safari_web_id: "web.onesignal.auto.028d9952-ba2c-477b-babc-6aee5c5ba0de",
-              notifyButton: {
-                enable: true,
-              },
-            });
-          });
-          console.log('OneSignalDeferred', window.OneSignalDeferred);
-        `}
-      </Script>
 
       {children}
     </>
